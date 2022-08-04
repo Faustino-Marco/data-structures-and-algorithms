084f0b53a38fb03abcbe1685e55030ec4ca34f1e
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-03 23:35:21 -0700

    pushing so I can make a new branch for submitting challenge 13

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -4,8 +4,23 @@ class PseudoQueue:
     def __init__(self):
         self.front = None
         self.rear = None
+        self.top = None
     
-    def enqueue(self, stack, value):
+    def enqueue(self, value):
+        stg_stk = Stack()
+        if Stack.is_empty() is not True:
+            if self.top.next == None:
+                self.top.next = value
+                return
+            else:
+                stg_stk.push(Stack.pop())
+
+
+
+
+
+
+
         new_rear = Node(value)
         if not self.rear:
             self.rear = new_rear
@@ -15,16 +30,17 @@ class PseudoQueue:
             old_rear.next = new_rear
             self.rear = new_rear
 
-    def dequeue(self, stack):
-        if not self.front:
-            raise InvalidOperationError
-        else:
-            # remove front
-            # reset front
-            # return value
-            old_front = self.front
-            self.front = self.front.next
-            return old_front.value
+    def dequeue(self):
+        Stack.pop()
+        # if not self.front:
+        #     raise InvalidOperationError
+        # else:
+        #     # remove front
+        #     # reset front
+        #     # return value
+        #     old_front = self.front
+        #     self.front = self.front.next
+        #     return old_front.value
 
 class Node:
     def __init__(self, value, next_=None):
