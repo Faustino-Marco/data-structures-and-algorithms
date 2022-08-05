83770237b7c3e1cb7a215686f6dbde53aff737eb
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-05 00:06:12 -0700

    push to finish and move on to animal shelter

--- python/tests/code_challenges/test_stack_queue_pseudo.py
+++ python/tests/code_challenges/test_stack_queue_pseudo.py
@@ -22,7 +22,7 @@ def test_enqueue_one():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_enqueue_two():
     pq = PseudoQueue()
     pq.enqueue("apples")
@@ -37,7 +37,7 @@ def test_enqueue_two():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_enqueue_dequeue_enqueue_dequeue():
     pq = PseudoQueue()
     pq.enqueue("apples")
