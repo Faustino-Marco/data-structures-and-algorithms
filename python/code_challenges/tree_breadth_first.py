0c91b2706ba1d356149b540545d6e9777ba9de87
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-08 23:33:39 -0700

    imported queue, included in fn() - unfinished

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,7 +1,19 @@
 from data_structures.binary_tree import BinaryTree, Node
-
+from data_structures.stack_and_queue.queue import Queue
 
 def breadth_first(tree):
+    """
+    Write a function called breadth_first
+    Arguments: tree
+    Return: list of all values in the tree, in the order they were encountered
+    """
+    breadth_queue = Queue()
+    breadth_queue.enqueue(tree.root)
+
+    
     values = []
-    values.append(tree.root.value)
+
+    def walk(root):
+        values.append(root.value)
+
     return values
--- python/tests/code_challenges/test_tree_breadth_first.py
+++ python/tests/code_challenges/test_tree_breadth_first.py
@@ -16,7 +16,7 @@ def test_rootless_tree():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_single_node():
     tree = BinaryTree()
     tree.root = Node("apples")
