c2e29ecf215770b4e87379d63daa8c93644f6d14
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-26 23:16:21 -0700

    first pass at hashtable following along with lecture recording

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,61 @@
+from data_structures.linked_list import LinkedList
+
 class Hashtable:
     """
     Put docstring here
     """
 
-    def __init__(self):
+    def __init__(self, size):
         # initialization here
-        pass
+        self.size = size
 
-    def some_method(self):
+    def get(self, key):
         # method body here
         pass
+
+    def set(self, key, value):
+        index = self.hash(key)
+        bucket = self.buckets[index]
+        if not bucket:
+            bucket = LinkedList()
+            self.buckets[index] = bucket
+        kvp = (key, value)
+        bucket.insert(kvp)
+
+    def contains(self, key):
+        index = self.hash(key)
+        bucket = self.buckets[index]
+        if not bucket:
+            return False
+
+        current = bucket.head
+        while current:
+            kvp = current.value
+            if kvp[0] == key:
+                return True
+            current = current.next
+        
+        return False
+
+    def keys(self):
+        gathered_keys = []
+        for bucket in self.buckets:
+            if bucket:
+                current = bucket.head
+                while current:
+                    kvp = current.value
+                    gathered_keys.append(kvp[0])
+        return gathered_keys
+
+
+    def hash(self, key):
+        # add ascii values of each char together
+        ascii_values = [ord(char) for char in key]
+        ascii_sum = sum(ascii_values)
+
+        primed = ascii_sum * 599
+        index = primed % self.size
+
+        return index # in range aka between 0 and the size - 1
+
+
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -5,6 +5,42 @@ from data_structures.hashtable import Hashtable
 def test_exists():
     assert Hashtable
 
+def test_get_returns_none_for_missing_key():
+    hashtable = Hashtable()
+    actual = hashtable.get("spam")
+    expected = None
+    assert actual == expected
+
+def test_buckets_size():
+    table = Hashtable()
+    actual = len(table.buckets)
+    expected = 1024
+    assert actual == expected
+    
+def test_hash_in_range():
+    table = Hashtable(size=2)
+    keys = ["spam", "eggs", "bacon", "toast"]
+    for key in keys:
+        index = table.hash(key)
+        assert 0 <= index < table.size
+
+def test_set_does_not_blow_up():
+    table = Hashtable()
+    table.set("eggs", "bacon")
+    assert True
+
+def test_keys():
+    table = Hashtable()
+    table.set("eggs", "bacon")
+    table.set("ping", "pong")
+    keys = table.keys()
+    actual = keys
+    expected = ["eggs", "bacon"]
+    assert actual == expected 
+    
+@pytest.mark.skip("TODO")
+def test_buckets_size():
+    table = Hashtable()
 
 @pytest.mark.skip("TODO")
 def test_get_apple():
