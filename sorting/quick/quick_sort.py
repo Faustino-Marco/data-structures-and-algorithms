a496c6b2a81a88a23574e78737ad94b6cdc4dd94
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-10-03 01:28:32 -0500

    code / tests for quicksort

---
+++ sorting/quick/quick_sort.py
@@ -0,0 +1,23 @@
+def quick_sort(nums, left, right):
+    if left < right:
+        position = partition(nums, left, right)
+        quick_sort(nums, left, position - 1)
+        quick_sort(nums, position + 1, right)
+
+    return nums
+
+def piece(nums, left, right):
+    turn = nums[right]
+    low = left - 1
+
+    for i in range(left, right):
+        if nums[i] <= turn:
+            low += 1
+            swap(nums, i, low)
+
+    swap(nums, right, low + 1)
+
+    return low + 1
+
+def switch(nums, i, low):
+        nums[i], nums[low] = nums[low], nums[i]
